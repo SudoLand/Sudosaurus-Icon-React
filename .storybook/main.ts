fbb5e0a8d2642e525917da79e093eadbda6a7fe2
wmxpy <wm@mengw.io>
2023-12-15 21:02:29 -0600

    Update .gitignore and .storybook/main.ts, and package.json dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,24 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
+    options: {}
+  },
+  features: {
+    "storyStoreV7": true
   },
   docs: {
-    autodocs: "tag",
-  },
+    autodocs: true,
+  }
 };
 export default config;
